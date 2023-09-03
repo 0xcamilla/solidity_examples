55260c674a1685f7e1db0134621916e9a40d1d7b
0xcamilla <143549537+0xcamilla@users.noreply.github.com>
2023-09-03 19:43:23 +0300

    Create clock.sol

---
+++ clock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+contract Kata {
+  function past(int h, int m, int s) public pure returns (int) {
+    // your code here
+  }
+}
